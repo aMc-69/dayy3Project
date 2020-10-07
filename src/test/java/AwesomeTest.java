36707a961a68067abec80cacdc1b85fa67b7e239
AMetinCetin <CT.AMC.69@gmail.com>
2020-10-07 15:57:28 +0200

    3rd commit added sentences

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -1,5 +1,9 @@
 public class AwesomeTest {
     public static void main(String[] args) {
         System.out.println("awesome printed");
+
+        // red means untracked
+        // green means under staging area
+        // grey in .idea folder means it's ignored
     }
 }
